4cee973870a84ffcc52b54e659e1d71c1118414c
Prakash <prakash.hartron@gmail.com>
2018-05-06 14:58:03 +0530

    Container with text child

--- lib/main.dart
+++ lib/main.dart
@@ -7,23 +7,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter App',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+        title: 'Flutter App',
+        theme: new ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
+          // counter didn't reset back to zero; the application is not restarted.
+          primarySwatch: Colors.blue,
+        ),
 //      home: new MyHomePage(title: 'Flutter Demo Home Page'),
 //      home: new Banner(message: "Hello Word", location: BannerLocation.bottomEnd),
-        home: new Text(
-          "Hello Android App",textAlign: TextAlign.center,overflow: TextOverflow.ellipsis),
-    );
+//        home: new Text(
+//          "Hello Android App",textAlign: TextAlign.center,overflow: TextOverflow.ellipsis),
+        home: new Container(
+            color: Colors.white,
+            padding: const EdgeInsets.all(50.9),
+            alignment: Alignment.topCenter,
+            child: new Text(
+              "Hello App",
+              textAlign: TextAlign.center,
+              textScaleFactor: 0.8,
+              style: TextStyle(color: Colors.black),
+            )));
   }
 }
 
